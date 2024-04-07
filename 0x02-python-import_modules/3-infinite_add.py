31df4e2385bd143e23c509ea5330478dd38457af
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-08 00:30:48 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import sys
 
 
 def infinite_add():
-    args = sys.argv[1:]
-    total = sum(map(int, args))
-    print(total, end='\n')
+    num_args = len(sys.argv[1:])
+    if num_args == 0:
+        print('{:d}'.format(0))
+    else:
+        int_argument = [int(arg) for arg in sys.argv[1:]]
+        result = 0
+        for i in range(0, num_args):
+            result += int_argument[i]
+        print(result)
 
 
 if __name__ == "__main__":
