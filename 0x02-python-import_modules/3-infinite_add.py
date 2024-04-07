12fd57817225a7f1886282fbe30794ff103fb009
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-08 00:23:38 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 import sys
 
-if __name__ == "__main__":
+
+def infinite_add():
     args = sys.argv[1:]
     result = sum(int(arg) for arg in args)
     print(result)
+
+
+if __name__ == "__main__":
+    infinite_add()
