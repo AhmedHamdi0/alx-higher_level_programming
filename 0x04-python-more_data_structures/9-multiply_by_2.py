e36a0a93494452213002e1a96f88583831a6cc0a
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-08 00:52:27 +0200

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {}
-    for key, value in a_dictionary.items():
-        new_dict[key] = value * 2
-    return new_dict
-
+    return dict(map(lambda item: (item[0], item[1] * 2), a_dictionary.items()))
