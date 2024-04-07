97dd0c5de6327ff1e21ff46df87f1f021de21e16
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-08 00:32:37 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import hidden_4
 
-for i in dir(hidden_4):
-    if i[:2] != '__':
-        print("{:s}".format(i))
+
+def hidden_discovery():
+    for i in dir(hidden_4):
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
+
+if __name__ == '__main__':
+    hidden_discovery()
