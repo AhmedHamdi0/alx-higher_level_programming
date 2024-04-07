14e74b2a5529768ce902c6767e49cab5e343d6d5
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-08 00:03:26 +0200

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(s):
+    result = ""
     for char in s:
         if 97 <= ord(char) <= 122:
             char = chr(ord(char) - 32)
-        print(char, end='')
-    print()
-
+        result += char
+    print(result)
