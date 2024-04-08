6b1ba0ea8a98036e301f58f9c51249416d78637d
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-09 00:45:34 +0200

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines the Square class."""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size):
+        """Initializes a square with a given size."""
+        self.__size = size
