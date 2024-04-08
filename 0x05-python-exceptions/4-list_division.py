a9d39bf85b53e62ed121e29a10ab9a0a4ae4e1f2
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-08 03:11:06 +0200

    Create 4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+# !/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+    try:
+        for i in range(list_length):
+            try:
+                div_result = float(my_list_1[i]) / float(my_list_2[i])
+            except ZeroDivisionError:
+                result_list.append(0)
+                print("division by 0")
+            except ValueError:
+                result_list.append(0)
+                print("wrong type")
+            except IndexError:
+                print("out of range")
+                break
+            else:
+                result_list.append(div_result)
+    finally:
+        return result_list
