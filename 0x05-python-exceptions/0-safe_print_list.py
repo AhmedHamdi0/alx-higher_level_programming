e28eceb2c75155081ae5afd5c664492c132003c1
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-08 03:10:55 +0200

    Create 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    printed_elements = 0
+    try:
+        for element in my_list[:x]:
+            print(element, end='')
+            printed_elements += 1
+    except IndexError:
+        pass
+    print()
+    return printed_elements
