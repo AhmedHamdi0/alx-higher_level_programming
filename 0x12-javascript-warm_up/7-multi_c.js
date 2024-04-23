9ea340446aa90e236316d0ee8349bd61fad59e13
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-23 02:41:20 +0200

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,10 +3,10 @@
 const arg = process.argv[2];
 const times = parseInt(arg, 10);
 
-if (!isNaN(times) && times > 0) {
+if (arg === undefined || isNaN(arg)) {
+  console.log('Missing number of occurrences');
+} else {
   for (let i = 0; i < times; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
