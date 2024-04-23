80a0bda4572e39a357295d6f25fd460bf328562b
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-23 02:37:04 +0200

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-const times = parseInt(process.argv[2], 10);
+const arg = process.argv[2];
+const times = parseInt(arg, 10);
 
 if (!isNaN(times) && times > 0) {
   for (let i = 0; i < times; i++) {
