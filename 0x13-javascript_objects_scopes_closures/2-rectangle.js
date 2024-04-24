b11f5c8990f06afd331fe1da44eefa52296e9423
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-24 03:24:26 +0200

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 
 class Rectangle {
-  width;
-  height;
-
-  constructor (w, h) {
+  constructor(w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
-      Object.assign(this, {});
+      return {};
     }
   }
 }
