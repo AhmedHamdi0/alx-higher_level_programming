d86b8ce5e97c018cb15c182a0f86bba845023efc
Ahmed Hamdi <ahmed88hamdi99@gmail.com>
2024-04-24 04:08:07 +0200

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,8 +6,8 @@ const fileA = process.argv[2];
 const fileB = process.argv[3];
 const destination = process.argv[4];
 
-const contentA = fs.readFileSync(fileA, 'utf8');
-const contentB = fs.readFileSync(fileB, 'utf8');
-const concatenatedContent = contentA + '\n' + contentB;
+const contentA = fs.readFileSync(fileA).toString();
+const contentB = fs.readFileSync(fileB).toString();
+const concatenatedContent = contentA + contentB;
 
 fs.writeFileSync(destination, concatenatedContent);
