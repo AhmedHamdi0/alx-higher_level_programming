a0276394928b2029226dd174032d2055af611c5a
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2024-04-29 20:50:12 +0300

    update too long lines

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,10 +7,12 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database> <state_name>".
+              format(sys.argv[0]))
         sys.exit(1)
 
-    username, password, db_name, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    username, password, db_name, state_name = (
+        sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(username, password, db_name))
--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,13 +10,16 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(State.name, City.id, City.name).filter(State.id == City.state_id)
+    cities = (session.query(State.name, City.id, City.name).
+              filter(State.id == City.state_id))
     for city in cities:
         print(city[0] + ": (" + str(city[1]) + ") " + city[2])
 
